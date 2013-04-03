11e71cfc3f14dd801d0724fe1160b1da8b3e0f90
notifyer <ls5231@gmail.com>
2013-04-03 09:48:44 +0800

    Update db-import.groovy

--- db-import.groovy
+++ db-import.groovy
@@ -191,10 +191,10 @@ class Table {
         }
         
         def end = System.nanoTime();
-        println "导入成功..., 耗时: " + (end - start) /1000000 + "毫秒\n\r\n\r";
+        println "导入成功..., 耗时: " + (end - start) /1000000 + "毫秒\n\n";
         
         //级联更新
-        this.cascadeSons.each{
+        cascadeSons.each{
             def t = it;
             println "级联更新: " + it.table
             rowMaps.each{
