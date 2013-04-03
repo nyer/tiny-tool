f93582cba555a108ce82766456d9e093e6f5ddc7
notifyer <ls5231@gmail.com>
2013-04-03 09:56:17 +0800

    Update db-import.groovy

--- db-import.groovy
+++ db-import.groovy
@@ -194,8 +194,7 @@ class Table {
         
         def end = System.nanoTime();
         
-        def timeConsuming = ((end - start) /1000000);
-        println "导入成功..., 耗时: ${timeConsuming} 毫秒\n\n";
+        def timeConsuming = ((end - start) / 1000000);
         
         //级联更新
         this.cascadeSons.each{
